7b10660f53837eaf70c0e5993142c3c2bb955112
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-08-27 21:33:35 +0300

    feat: add not-found page but id doesn't work yet

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,5 +1,11 @@
-import { NotFoundPage } from "@pages/NotFoundPage";
+import Link from "next/link";
 
 export default function NotFound() {
-  return <NotFoundPage />;
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
 }
