8f32d705cdb899a2ce309611f868509a643023fd
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-09-03 23:47:50 +0300

    feat: add layout and info for Welcome Page

--- src/app/[lang]/welcome/page.tsx
+++ src/app/[lang]/welcome/page.tsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
 import { Locale } from "../../../../i18n-config";
 import { getDictionary } from "../../../app/[lang]/dictionaries";
+import nadyavalin from "../../../../public/nadyavalin.jpg";
+import katika from "../../../../public/ifbfirst.jpg";
+import larry from "../../../../public/LarryDavidd.jpg";
 
 export const WelcomePage = async ({ params: { lang } }: { params: { lang: Locale } }) => {
   const dictionary = await getDictionary(lang);
@@ -7,9 +12,53 @@ export const WelcomePage = async ({ params: { lang } }: { params: { lang: Locale
   return (
     <>
       <main>
-        <h1>Welcome Page</h1>
-        <p>{dictionary.home.title}</p>
-        <p>{dictionary.home.desc}</p>
+        <section className={styles.sectionDevelopers}>
+          <h1>Welcome Page</h1>
+          <div className={styles.developers}>
+            <div className={styles.developer}>
+              <Image
+                src={katika}
+                alt="katika"
+                width="250"
+                height="250"
+                className={styles.developerImage}
+              />
+              <p className={styles.developerName}>{dictionary.developers.katika.name}</p>
+              <p className={styles.developerPosition}>{dictionary.developers.katika.position}</p>
+              <p>{dictionary.developers.katika.desc}</p>
+            </div>
+
+            <div className={styles.developer}>
+              <Image
+                src={larry}
+                alt="LarryDavidd"
+                width="250"
+                height="250"
+                className={styles.developerImage}
+              />
+              <p className={styles.developerName}>{dictionary.developers.LarryDavidd.name}</p>
+              <p className={styles.developerPosition}>
+                {dictionary.developers.LarryDavidd.position}
+              </p>
+              <p>{dictionary.developers.LarryDavidd.desc}</p>
+            </div>
+
+            <div className={styles.developer}>
+              <Image
+                src={nadyavalin}
+                alt="nadyavalin"
+                width="250"
+                height="250"
+                className={styles.developerImage}
+              />
+              <p className={styles.developerName}>{dictionary.developers.nadyavalin.name}</p>
+              <p className={styles.developerPosition}>
+                {dictionary.developers.nadyavalin.position}
+              </p>
+              <p>{dictionary.developers.nadyavalin.desc}</p>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
