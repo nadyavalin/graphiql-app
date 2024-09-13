889e2dfecc392dd3014f35a233fd368f639fa203
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-09-13 16:21:03 +0300

    feat: style scrollbar for app

--- src/tests/__mocks__/firebase.ts
+++ src/tests/__mocks__/firebase.ts
@@ -2,3 +2,6 @@ import { vi } from "vitest";
 
 export const getAuth = vi.fn();
 export const initializeApp = vi.fn();
+export const signInWithEmailAndPassword = vi.fn(() => {
+  return Promise.resolve({ user: { email: "test@example.com" } });
+});
