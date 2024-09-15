dc62b1adfb2300d2675fdc43ac8fd17e8afea566
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-09-15 11:15:11 +0300

    feat: add test for logout feature

--- src/components/shared/hooks/useAppDispatch/index.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@shared/store";
-
-const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export default useAppDispatch;
--- src/tests/__mocks__/firebase.ts
+++ src/tests/__mocks__/firebase.ts
@@ -3,5 +3,5 @@ import { vi } from "vitest";
 export const getAuth = vi.fn();
 export const initializeApp = vi.fn();
 export const signInWithEmailAndPassword = vi.fn(() => {
-  return Promise.resolve({ user: { email: "test@example.com" } });
+  return Promise.resolve({ user: { email: "example@gmail.com", password: "123" } });
 });
